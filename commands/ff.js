b19a7bfb237e78a2cf9a731f834f8f61f35198b6
Kevin Humfreville <khumfreville@verizon.net>
2021-05-17 15:56:04 -0700

    test

--- commands/ff.js
+++ commands/ff.js
@@ -5,18 +5,6 @@ module.exports = {
     description: 'Some information regarding Fearless-Fighters.',
     category: 'Informational',
     callback: async ({ client, interaction, message }) => {
-
-        var user = await client.api.applications(client.user.id).users(interaction.member.user.id).get();
-        console.log(user);
-        //var user = client.api.applications(client.user.id).users(interaction.user.id).get();
-        console.log('--------------');
-        //console.log(user);
-
-
-
-
-/////
-
         const response = 
         'Website: https://fearless-fighters.engine.com\n' +
         'Teamspeak: fearless-fighters.engine.com (password: FFMPT)\n';
