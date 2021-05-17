5b6855ec901adf48bbc17bc2ebff482ba1001ee5
Kevin Humfreville <khumfreville@verizon.net>
2021-05-17 10:42:07 -0700

    Revert

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
-//const WOKCommands = require('wokcommands');
-//require('dotenv').config();
+const WOKCommands = require('wokcommands');
+require('dotenv').config();
 //const db = require('./util/db');
 
 const client = new Discord.Client({
@@ -10,13 +10,12 @@ const client = new Discord.Client({
 client.on('ready', async () => {
   //await db();
 
-  //new WOKCommands(client, { 
-  //  commandsDir: './commands',
-  //  showWarnings: false,
-  //});
+  new WOKCommands(client, { 
+    commandsDir: './commands',
+    showWarnings: false,
+  });
 
   console.log('RavenBot is ready.');
 });
 
-client.login('ODQyMDg5OTA5MzEyMjkwODI2.YJwPVw.mvqaMmcaaKvSNhazmVGvaasBdGo');
-//client.login(process.env.BOT_TOKEN);
+client.login(process.env.BOT_TOKEN);
