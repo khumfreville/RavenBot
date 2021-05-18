5391b6b22fc9f8e4cf70be6478d4b74b28622bfc
Kevin Humfreville <khumfreville@verizon.net>
2021-05-18 09:26:36 -0700

    debugging

--- commands/configure.js
+++ commands/configure.js
@@ -15,95 +15,87 @@ module.exports = {
         const userId = interaction.member.user.id;
         var botOwners = '';
         
-        await mongo().then(async (db) => {
+        return await mongo().then(async (db) => {
             botOwners = (await db.getSetting('botowner')).value;
-        }).catch(err => {
-            console.log(err);
-        });
-
-        if (command != 'set' && setting != 'botowner') {
-            if (botOwners == null || !botOwners || botOwners.length == 0) {
-                return '<@' + interaction.member.user.id + '>, no owner has been set!  Please do so with "/configure set botowner <USER_ID>"';
-            }
-        
-            if (botOwners && !botOwners.includes(userId)) {
-                return '<@' + interaction.member.user.id + '>, this function is for bot owners only.';
+        }).then(async (botOwners) => {
+            if (command != 'set' && setting != 'botowner') {
+                if (botOwners == null || !botOwners || botOwners.length == 0) {
+                    return '<@' + interaction.member.user.id + '>, no owner has been set!  Please do so with "/configure set botowner <USER_ID>"';
+                }
+            
+                if (botOwners && !botOwners.includes(userId)) {
+                    return '<@' + interaction.member.user.id + '>, this function is for bot owners only.';
+                }
             }
 
-            //return '<@' + interaction.member.user.id + '>, I got lost... please try again.';
-        }
-
-        var settings = [];
-        await mongo().then(async (db) => {
-            await db.getAllSettings()
-                .then(async (results) => {
-                    await results.forEach((item) => {
-                        settings.push(item.name);
+            var settings = [];
+            await mongo().then(async (db) => {
+                await db.getAllSettings()
+                    .then(async (results) => {
+                        await results.forEach((item) => {
+                            settings.push(item.name);
+                        });
                     });
-                });
+            }).catch(err => {
+                console.log(err);
+            });
+
+            switch (command) {
+                default:
+                case 'help' : {
+                    const result = new MessageEmbed();
+                    result.setColor('C92C2C');
+                    result.setTitle('RavenBot Configuration');
+                    result.description = 'RavenBot Commands:';
+                    result.addField('set | get', 'Sets or gets the value of field.');
+                    result.addField('Variables', settings);
+
+                    // If you want to send it to the user directly rather than in the public chat:
+                    // var u = await client.users.fetch(interaction.member.user.id);
+                    // u.send(result);
+
+                    // If you want to send it to public chat:        
+                    return result;
+                }
+
+                case 'set': {
+                    return await mongo().then(async(db) => {
+                        await db.putSetting(setting, value);
+                    }).then(result => {
+                        if (result) {
+                            return '<@' + interaction.member.user.id + '>, ' + setting + ' value updated.';
+                        }
+                        else {
+                            return '<@' + interaction.member.user.id + '>, Unable to update value for ' + setting + '.';
+                        }
+                    }).catch(err => {
+                        console.log(err);
+                    });
+                }
+
+                case 'get': {
+                    // Since we already have the 'botowners' value, just return it.
+                    if  (setting == 'botowner') {
+                        return setting + ' = ' + botOwners;
+                    };
+
+                    return await mongo()
+                        .then(async (db) => {
+                            return await db.getSetting(setting);
+                        }).then(result => {
+                            if  (result) {
+                                return '<@' + interaction.member.user.id + '>, ' + setting + ' = ' + result.value;
+                            }
+                            else {
+                                return  '<@' + interaction.member.user.id + '>, Setting not found: \"' + setting + '\".\nAvailable settings are: ' + settings;
+                            }
+                        }).catch(err => {
+                            console.log(err);
+                        });
+                }
+            }
         }).catch(err => {
             console.log(err);
         });
-
-        switch (command) {
-            default:
-            case 'help' : {
-                const result = new MessageEmbed();
-                result.setColor('C92C2C');
-                result.setTitle('RavenBot Configuration');
-                result.description = 'RavenBot Commands:';
-                result.addField('set | get', 'Sets or gets the value of field.');
-                result.addField('Variables', settings);
-
-                // If you want to send it to the user directly rather than in the public chat:
-                // var u = await client.users.fetch(interaction.member.user.id);
-                // u.send(result);
-
-                // If you want to send it to public chat:        
-                return result;
-            }
-
-            case 'set': {
-                var returnValue;
-                await mongo().then(async(db) => {
-                    await db.putSetting(setting, value);
-                }).then(result => {
-                    if (result) {
-                        returnValue = '<@' + interaction.member.user.id + '>, ' + setting + ' value updated.';
-                    }
-                    else {
-                        returnValue = '<@' + interaction.member.user.id + '>, Unable to update value for ' + setting + '.';
-                    }
-                }).catch(err => {
-                    console.log(err);
-                });
-
-                return returnValue;
-            }
-
-            case 'get': {
-                var returnValue;
-
-                // Since we already have the 'botowners' value, just return it.
-                if  (setting == 'botowner') {
-                    return setting + ' = ' + botOwners;
-                };
-
-                await mongo().then(async (db) => {
-                    await db.getSetting(setting);
-                }).then(result => {
-                    if  (result) {
-                        returnValue = '<@' + interaction.member.user.id + '>, ' + setting + ' = ' + result.value;
-                    }
-                    else {
-                        returnValue = '<@' + interaction.member.user.id + '>, Setting not found: \"' + setting + '\".\nAvailable settings are: ' + settings;
-                    }
-                }).catch(err => {
-                    console.log(err);
-                });
-
-                return returnValue;
-            }
-        }
     }
 };
