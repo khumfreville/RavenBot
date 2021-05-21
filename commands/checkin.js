8c2c1d5e737e8411a4e0e7b3e985f70eaa8e8d15
Kevin Humfreville <khumfreville@verizon.net>
2021-05-21 13:02:30 -0700

    bug fix

--- commands/checkin.js
+++ commands/checkin.js
@@ -35,7 +35,7 @@ module.exports = {
                 if (content === 'clear') {
                     // Clear the channel.
                     await client.channels.cache.get(rollCallChannelId).bulkDelete(100)
-                        .then(() => {
+                        .then(async () => {
                             // Add the "Roll Call Message".
                             await db.getSetting(guild.id, 'rollcallmessage')
                                 .then(async (value) => {
